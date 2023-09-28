5f191223b76b5a4fb27eedd414a45b8e545793a0
Wander S. Moschetta <wander@familiaprever.com.br>
2023-09-28 16:36:23 -0300

    importado db em users
    importado db em users

--- routes/users.js
+++ routes/users.js
@@ -1,10 +1,11 @@
 var express = require('express');
 var router = express.Router();
+const db = require('../database/db');
 
 
-/* GET users listing. */
+
 router.get('/', async function(req, res) {
-  const usuarios = ''; //await prisma.usuario.findMany();
+  const usuarios = await  db.sql.query('select top 10 * from Usuarios order by Nome');
   
   res.json(usuarios);
 });
